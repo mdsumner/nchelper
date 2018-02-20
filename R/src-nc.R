fde8004eec7789816c2dcde6f8279f398e173786
Michael Sumner <mdsumner@gmail.com>
2018-02-21 09:25:33 +1100

    tbl/src

--- R/src-nc.R
+++ R/src-nc.R
@@ -1,26 +1,42 @@
-#' @importFrom dplyr src
-src_nc <- function(con, ...) {
-  x <- src("nc") 
-  x$con <- con
-  x
+
+tbl_nc <- function(variable) {
+  tibble::as_tibble(setNames(list(RNetCDF::var.get.nc(RNetCDF::open.nc(ncsource), variable)), 
+                             variable))
 }
-#' @importFrom dplyr db_list_tables
-db_list_tables.src_nc <- function(con) {
-  ncmeta::nc_vars(con$con)$name
+src_tbls.src_nc <- function(x) {
+  ncmeta::nc_vars(x$ncsource)$name
 }
 print.src_nc <- function(x, ...) {
-  cat(sprintf("NetCDF source: %s\n  available variables:\n %s ", 
-              x$con,
-              paste(db_list_tables(nc), collapse = ", ")))  
+  print(x$name)
+  print(src_tbls(x))
 }
+#' Title
+#'
+#' @param ncsource 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' f <- system.file("extdata", "S2008001.L3m_DAY_CHL_chlor_a_9km.nc", package = "ncmeta")
+#' src_nc(f)
+src_nc <- function(ncsource) {
+  structure(list(tbl_f = tbl_nc,
+                 name = sprintf("<NetCDF: %s>", ncsource),
+                 ncsource = ncsource),
+            class = c("src_nc", "src")
+  )
+}
+
+
 #' @importFrom dplyr tbl
-tbl.src_nc <- function(x, from, ...) {
-  if (missing(from)) stop("need 'from' variable name")
-  stopifnot(from %in% db_list_tables(x))
-  a <- nchelper(x$con, from)
-  dm <- dim(a)
-  starts <- rep(1, length(dm))
-  counts <- starts
-  counts[1] <- 6
-  tibble::as_tibble(setNames(list(RNetCDF::var.get.nc(RNetCDF::open.nc(x$con), from, start = starts, count = counts)), from))
-}
+# tbl.src_nc <- function(x, from, ...) {
+#   if (missing(from)) stop("need 'from' variable name")
+#   stopifnot(from %in% db_list_tables(x))
+#   a <- nchelper(x$con, from)
+#   dm <- dim(a)
+#   starts <- rep(1, length(dm))
+#   counts <- starts
+#   counts[1] <- 6
+#   tibble::as_tibble(setNames(list(RNetCDF::var.get.nc(RNetCDF::open.nc(x$con), from, start = starts, count = counts)), from))
+# }
